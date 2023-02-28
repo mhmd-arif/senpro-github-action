4c976e47807fc54e048abef85709cd0961a34fd5
mhmd-arif <mhmd.arif.developer@gmail.com>
2023-02-28 11:40:41 +0700

    rollback case3 to see in VM

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import { Inter } from '@next/font/google'
+// import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
+  return (  
     <>
       <Head>
         <title>Modul 01 - Lab 2.2 | Senior Project 2023</title>
